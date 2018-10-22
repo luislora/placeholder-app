de89ffa8be8717ba5db619d640ac82ebd66bc997
Luis Lora <luisgabriellora@gmail.com>
2018-10-22 16:11:54 -0500

    Added new styles and fixes

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 A placeholder project bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+
+And Redux
--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,13 +1,22 @@
 import React, {Component} from 'react';
+import '../styles/counter.css';
 
 export default class Counter extends Component {
     render() {
         const {count, onIncreaseClick} = this.props;
 
         return (
-            <div>
-                <span>{count}</span>
-                <button onClick={onIncreaseClick}>Increase</button>
+            <div className="second-container">
+                <div className="row justify-content-md-center">
+                    <div className="col-md-1 count-label">
+                        <span>{count}</span>
+                    </div>
+                </div>
+                <div className="row justify-content-md-center">
+                    <div className="col-md-1">
+                        <button className="btn btn-primary" onClick={onIncreaseClick}>Increase</button>
+                    </div>
+                </div>
             </div>
         );
     }
