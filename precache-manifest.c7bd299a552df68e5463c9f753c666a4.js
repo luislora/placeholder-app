07f58fbc0741b9b1c785c8003f030322c3a6de99
Luis Lora <luisgabriellora@gmail.com>
2018-10-22 17:03:31 -0500

    Updates

---
+++ precache-manifest.c7bd299a552df68e5463c9f753c666a4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8bf9b06358c6e284cafe",
+    "url": "/placeholder-app/static/js/runtime~main.8bf9b063.js"
+  },
+  {
+    "revision": "30c3576b4907d7ece68f",
+    "url": "/placeholder-app/static/js/main.30c3576b.chunk.js"
+  },
+  {
+    "revision": "6167bff16c1248152031",
+    "url": "/placeholder-app/static/js/1.6167bff1.chunk.js"
+  },
+  {
+    "revision": "30c3576b4907d7ece68f",
+    "url": "/placeholder-app/static/css/main.3ad83ae0.chunk.css"
+  },
+  {
+    "revision": "b685f10016beac0d339853ff92e592f7",
+    "url": "/placeholder-app/index.html"
+  }
+];
